c422f689e826ae9f1b5a729ed497314c5bb85458
loovee <luweicong@seeedstudio.com>
2015-09-24 22:38:29 +0800

    first sygn

---
+++ examples/test_seeed_gp/test_seeed_gp.ino
@@ -0,0 +1,22 @@
+// test for seeed graphical programming project
+// loovee @ 2015-8-4
+
+#include "TM1637.h"
+
+const int pinClk = 2;
+const int pinDta = 3;
+
+TM1637 led;
+
+void setup()
+{
+}
+
+void loop()
+{
+    for(int i=0; i<10000; i++)
+    {
+        led.DigitDisplayWrite(pinClk, pinDta, i);
+        delay(10);
+    }
+}
